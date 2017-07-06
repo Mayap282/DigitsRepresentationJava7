033621e61b69542432ce890d0500db50f4107a37
MayaP282 <maya.aitpune@gmail.com>
2017-07-05 19:59:53 -0700

    New feature of Java 7,underscore between the digits for more readability.

---
+++ Digits_java7/src/edu/com/digits/DigitsRepresantation.java
@@ -0,0 +1,12 @@
+package edu.com.digits;
+
+public class DigitsRepresantation {
+
+	public static void main(String[] args) {
+		
+		int a = 9_00000;
+		System.out.println("In Java7 uderscore between digits in numeric literal : "+a);
+
+	}
+
+}
